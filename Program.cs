e56f179daeeaee82f3b5f466ca2621742bea5fa2
sainani3067 <ammu97938@gmail.com>
2021-10-04 13:06:57 +0530

    day15binarysearchproblems

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Day15BinarySearchTreeproblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******Welcome to Binary Serach Tree******");
+            #region UC1
+            //UC1_problem1<int> BinarySearchtree = new UC1_problem1<int>(56);
+            //BinarySearchtree.Insert(30);
+            //BinarySearchtree.Insert(70);
+            //BinarySearchtree.Display();
+            #endregion
+
+            #region UC2
+            //UC2_problem2-<int> BinaryTree = new UC2problem2<int>(56);
+            //BinaryTree.Insert(30);
+            //BinaryTree.Insert(70);
+           //BinaryTree.Insert(22);
+            //BinaryTree.Insert(40);
+            //BinaryTree.Insert(60);
+            //BinaryTree.Insert(95);
+            //BinaryTree.Insert(11);
+            //BinaryTree.Insert(65);
+            //BinaryTree.Insert(3);
+            //BinaryTree.Insert(16);
+            //BinaryTree.Insert(63);
+            //BinaryTree.Insert(67);
+            //BinaryTree.Display();
+            //BinaryTree.Getsize();
+            #endregion
+
+            #region UC3
+            //UC3_problem3<int> Binarytree = new UC3_problem3<int>(56);
+            //Binarytree.Insert(30);
+            //Binarytree.Insert(70);
+            //Binarytree.Insert(22);
+            //Binarytree.Insert(40);
+            //Binarytree.Insert(60);
+            //Binarytree.Insert(95);
+            //Binarytree.Insert(11);
+            //Binarytree.Insert(65);
+            //Binarytree.Insert(3);
+            //Binarytree.Insert(16);
+            //Binarytree.Insert(63);
+            //Binarytree.Insert(67);
+            //Binarytree.Display();
+            //Binarytree.Getsize();
+            //bool result = Binarytree.IfExists(63, Binarytree);
+            //Console.WriteLine(result);
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
+     
